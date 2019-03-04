11f90b3db9ebb886e3eae796d96d7e364e0be5ad
Miguel BEcerra <miguelbecerra01@gmail.com>
2019-03-04 14:24:22 -0300

    Init commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//http://sass-lang.com/guide
+import './styles/styles.scss';
+import 'normalize.css/normalize.css';
+
+
+ReactDOM.render(<p>boilerplate</p>, document.getElementById('app'));
+
+
